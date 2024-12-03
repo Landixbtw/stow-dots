ac18fc82164c294e25ab6a40c07c5396af150b29
landixbtw <landixbtw@gmail.com>
2024-12-03 23:40:55 +0100

    Signed-off-by: landixbtw <landixbtw@gmail.com>

--- .wezterm.lua
+++ .wezterm.lua
@@ -5,9 +5,10 @@ local config = wezterm.config_builder()
 config.color_scheme = 'Afterglow'
 
 -- config.font = wezterm.font('CaskaydiaCove Nerd Font Mono', { weight = 'Bold' })
-config.font = wezterm.font('Iosevka Nerd Font Mono', { weight = 'Bold'})
+-- config.font = wezterm.font('Iosevka Nerd Font Mono', { weight = 'Bold'})
+config.font = wezterm.font('MartianMono Nerd Font', { weight = 'Regular'})
 
-config.font_size = 14
+config.font_size = 13
 
 config.enable_kitty_graphics = true
 
