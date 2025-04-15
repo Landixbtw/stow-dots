59226b586bff985f169d9ea59dd08f7c591148c9
landixbtw <landixbtw@gmail.com>
2025-04-15 07:56:12 +0200

    inward serach for emacs with cntrl + leftclick Signed-off-by: landixbtw <landixbtw@gmail.com>

--- emacs/.emacs.d/auctex.el
+++ emacs/.emacs.d/auctex.el
@@ -1,13 +1,22 @@
-   ;; latexmk
+;;; auctex-config.el --- Complete AUCTeX configuration for Emacs
+
+;;; Commentary:
+;; This configuration sets up AUCTeX with SyncTeX support,
+;; company completion, reftex, cdlatex, and Zathura as PDF viewer
+;; with both forward and inverse search functionality.
+
+;;; Code:
+
+;; Basic packages
 (use-package auctex-latexmk)
-;; company
 (use-package company-math)
 (use-package company-auctex)
 (use-package company-reftex)
-;;  use cdlatex
 (use-package cdlatex)
-;; https://gist.github.com/saevarb/367d3266b3f302ecc896
-;; https://piotr.is/2010/emacs-as-the-ultimate-latex-editor/
+
+;; Start Emacs server for synctex inverse search
+(server-start)
+
 (use-package latex
   :straight auctex
   :ensure auctex
@@ -22,7 +31,8 @@
   ("\\.tex\\'" . latex-mode)
   :bind (:map LaTeX-mode-map
               ("C-c C-e" . cdlatex-environment)
-              ("C-c C-v" . my-view-tex-file)  ;; Add custom viewing function binding
+              ("C-c C-v" . my-view-tex-file)
+              ("C-c C-g" . TeX-view) ;; Standard forward search
               )
   :hook
   (LaTeX-mode . olivetti-mode)
@@ -36,20 +46,28 @@
   (LaTeX-mode . try/latex-mode-setup)
   (LaTeX-mode . turn-on-cdlatex)
   :config
+  ;; Basic AUCTeX configuration
   (setq TeX-auto-save t)
   (setq TeX-parse-self t)
   (setq TeX-save-query nil)
   (setq reftex-plug-into-AUCTeX t)
-  ;; pdftools
-  ;; https://emacs.stackexchange.com/questions/21755/use-pdfview-as-default-auctex-pdf-viewer#21764
-  (setq TeX-view-program-selection '((output-pdf "Zathura"))
-        TeX-view-program-list '(("Zathura" "cd %s && zathura --synctex-forward %n:0:%b %o"))
-        TeX-source-correlate-start-server t)  ;; to have the buffer refresh after compilation,
-  ;; very important so that PDFView refesh itself after comilation
+  (setq TeX-output-dir ".")
+  
+  ;; SyncTeX configuration
+  (setq TeX-source-correlate-method 'synctex)
+  (setq TeX-source-correlate-mode t)
+  (setq TeX-source-correlate-start-server t)
+  
+  ;; PDF viewer configuration (Zathura with SyncTeX support)
+  (setq TeX-view-program-selection '((output-pdf "Zathura")))
+  (setq TeX-view-program-list
+        '(("Zathura" "zathura --synctex-editor-command 'emacsclient +%%{line} %%{input}' --synctex-forward %n:0:%b %o")))
+  
+  ;; Refresh buffer after compilation
   (add-hook 'TeX-after-compilation-finished-functions
             #'TeX-revert-document-buffer)
-  (setq TeX-output-dir ".")
-  ;; latexmk
+  
+  ;; latexmk setup
   (require 'auctex-latexmk)
   (auctex-latexmk-setup)
   (setq auctex-latexmk-inherit-TeX-PDF-mode t)
@@ -64,10 +82,12 @@
                              (file-name-nondirectory master-file)) ".pdf"))
            (pdf-path (expand-file-name pdf-file master-dir)))
       (message "Opening PDF: %s" pdf-path)
-      (start-process "zathura" nil "zathura" pdf-path)))
-  )
+      (start-process "zathura" nil "zathura" 
+                    "--synctex-editor-command" "emacsclient +%{line} %{input}"
+                    pdf-path)))
+)
 
-;; Add this to your init file
+;; Load required TeX modules
 (with-eval-after-load 'latex
   (require 'tex)
   (require 'tex-buf)
@@ -77,5 +97,23 @@
   (require 'font-latex)
   (require 'preview))
 
+;; Custom function to set up LaTeX mode (define this if not already defined)
+(defun try/latex-mode-setup ()
+  "Custom setup for LaTeX mode."
+  (setq-local company-backends
+              (append '((company-math-symbols-latex
+                         company-latex-commands
+                         company-auctex-macros
+                         company-auctex-environments
+                         company-auctex-symbols
+                         company-reftex-citations
+                         company-reftex-labels))
+                      company-backends)))
+
+;; Make sure zathurarc is properly configured
+;; Add this comment as a reminder to create ~/.config/zathura/zathurarc with:
+;; set synctex true
+;; set synctex-editor-command "emacsclient +%{line} %{input}"
+
 (provide 'auctex)
-;;; auctex.el ends here
+;;; auctex-config.el ends here
